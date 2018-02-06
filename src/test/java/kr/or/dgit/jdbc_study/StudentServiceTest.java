0a41b19b725a9a9c851de30af7e0bdf4379004ca
khj <khjin1231@gmail.com>
2018-02-06 13:11:04 +0900

    hotfix selectbyAll test

---
+++ src/test/java/kr/or/dgit/jdbc_study/StudentServiceTest.java
@@ -0,0 +1,32 @@
+package kr.or.dgit.jdbc_study;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import kr.or.dgit.jdbc_study.dto.Student;
+import kr.or.dgit.jdbc_study.service.StudentService;
+
+public class StudentServiceTest {
+	private static StudentService service;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		service = StudentService.getInstance();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		service = null;
+	}
+
+	@Test
+	public void testFindAllStudents() {
+		List<Student> list = service.findAllStudents();
+		//Assert.isNonEmpty(list);
+	}
+
+}
